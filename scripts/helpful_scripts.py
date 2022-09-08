fbf23d61d87c97f7f0eea634cb5a6eff584e77dc
khan-yaseen <yaseenkhan61@yahoo.com>
2022-09-09 00:01:13 +0530

    aimplemented functions to deploy contracts

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,12 @@
-from brownie import accounts, network, config
+from brownie import (
+    accounts,
+    network,
+    config,
+    MockV3Aggregator,
+    Contract,
+    VRFCoordinatorMock,
+    LinkToken,
+)
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
@@ -15,3 +23,54 @@ def get_account(index=None, id=None):
     ):
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+
+def get_contract(contract_name):
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, and
+    return that contract.
+
+        Args:
+            contract_name(string)
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if (len(contract_type)) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # name
+        # address
+        # ABI
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+
+    return contract
+
+
+DECIMALS = 8
+INITIAL_VALUE = 156000000000
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks...")
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+    link_token_address = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token_address, {"from": account})
+    print("Deployed!")
